6acfa9748db71911d90c128617285b0713cec672
lspazzoli <lspazzoli@gmail.com>
2016-07-21 12:18:02 +0200

    Fixed some bugs and system works on  zte phones

--- Code/Android/Dropoff/app/src/main/java/codeblox/dropoff/MainActivity.java
+++ Code/Android/Dropoff/app/src/main/java/codeblox/dropoff/MainActivity.java
@@ -1,15 +1,11 @@
 package codeblox.dropoff;
 
-import android.app.Dialog;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.google.android.gms.appindexing.Action;
@@ -22,10 +18,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity {
-    String vidAddress = "rtsp://admin:antigrav@192.168.1.119/profile3/media.smp";
-    String camIP="";
-    String piIP="";
-    Dialog PinDialog ;
+    String vidAddress = "rtsp://admin:antigrav@192.168.1.107/profile3/media.smp";
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -37,64 +30,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        super.onStart();
-        setUp();
-        setPin();
+        //Set up video feed
+        VideoView vidView = (VideoView) findViewById(R.id.myVideo);
+        Uri vidUri = Uri.parse(vidAddress);
+
+
+        //Controlls
+        MediaController vidControl = new MediaController(this);
+        vidControl.setAnchorView(vidView);
+        vidView.setMediaController(vidControl);
+
+        //Start Stream
+        vidView.setVideoURI(vidUri);
+        vidView.start();
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    private void setPin() {
-        PinDialog = new Dialog(this);
-        PinDialog.setContentView(R.layout.pin);
-        PinDialog.setTitle("Pin");
-
-        Button button = (Button) PinDialog.findViewById(R.id.OkayPin);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PinDialog.dismiss();
-            }
-        });
-    }
-
-    public void setUp()
-    { //Currently just for testing to get IP address of camera and pi
-        //ToDo Allow camer to steram throgh the pi and the pi will connect to the master server at later stage
-        final Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog);
-        dialog.setTitle("SetUp");
-
-        Button button = (Button) dialog.findViewById(R.id.Button01);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dialog.dismiss();
-                //Get TExt
-                EditText IP = (EditText)dialog.findViewById(R.id.IP);
-                EditText RBPIP = (EditText)dialog.findViewById(R.id.RBPIP);
-                 camIP = IP.getText().toString().trim();
-                 piIP = RBPIP.getText().toString().trim();
-
-                System.out.println("RBP "+RBPIP.getText().toString().trim()+"   Cam "+ IP.getText().toString().trim());
-                vidAddress = "rtsp://admin:antigrav@"+camIP+"/profile3/media.smp";
-
-                //Set up video feed
-                VideoView vidView = (VideoView) findViewById(R.id.myVideo);
-                Uri vidUri = Uri.parse(vidAddress);
-
-                //Start Stream
-                vidView.setVideoURI(vidUri);
-                vidView.start();
-                // ATTENTION: This was auto-generated to implement the App Indexing API.
-                // See https://g.co/AppIndexing/AndroidStudio for more information.
-
-
-
-            }
-        });
-
-        dialog.show();
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,23 +60,13 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.max://Setting video to profile 1 which shouold be the best quality
                 System.out.println("Max Enabled");
-                vidAddress = "rtsp://admin:antigrav@"+camIP+"/profile1/media.smp";
+                vidAddress = "rtsp://admin:antigrav@192.168.1.107/profile1/media.smp";
                 return true;
 
             case R.id.min://Setting video to profile 6 which shouold be the lowest quality
                 System.out.println("Min Enabled");
-                vidAddress = "rtsp://admin:antigrav@"+camIP+"/profile6/media.smp";
+                vidAddress = "rtsp://admin:antigrav@192.168.1.107/profile6/media.smp";
                 return true;
-
-            case R.id.pinBtn://Showing the pin
-                System.out.println("Pin Please");
-                TextView pin = (TextView)PinDialog.findViewById(R.id.pin);
-                pin.setText("The Curent Pin is "+ piIP);
-                PinDialog.show();
-                vidAddress = "rtsp://admin:antigrav@"+camIP+"/profile6/media.smp";
-                return true;
-
-
 
             case R.id.open:
                 // This will send a messages to the raspberry pi to open the gate
