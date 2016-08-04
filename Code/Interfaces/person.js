6edad799a829e3036f700605f3a1e330d4e07ef6
Bilal Muhammad <billibongers@gmail.com>
2016-08-04 12:42:08 +0200

    Made person.js a parent class

--- Code/Interfaces/person.js
+++ Code/Interfaces/person.js
@@ -10,41 +10,26 @@
 		CodeBlox
 *********************************************************************/
 
-function Person(_firstName, _lastName, _cellNumber, _idNumber, _email, _strAddress){
+function Person(_firstName, _lastName, _cellNumber){
 	this.firstName = _firstName;
 	this.lastName = _lastName;
-	this.cellNumber = _cellNumber;
-	this.idNumber = _idNumber;
-	this.email = _email;
-	this.strAddress = _strAddress; 
+	this.cellNumber = _cellNumber; 
 };
 
 Person.prototype.getName = function(){
-	return "The persons name is " + this.firstName + "\n";
+	return this.firstName;
 };
 
 Person.prototype.getLname = function(){
-	return "The persons last name is " + this.lastName + "\n";
+	return this.lastName;
 };
 
 Person.prototype.getCell = function(){
-	return "The persons cell is " + this.cellNumber + "\n";
-};
-
-Person.prototype.getIdNum = function(){
-	return "The persons id number is " + this.idNumber + "\n";
-};
-
-Person.prototype.getEmail = function(){
-	return "The persons email is " + this.email + "\n";
-};
-
-Person.prototype.getAddr = function(){
-	return "The persons Address is " + this.strAddress + "\n";
+	return this.cellNumber;
 };
 
 Person.prototype.getPerson = function(){
-	return this.getName() + this.getLname() + this.getCell() + this.getIdNum() + this.getEmail() + this.getAddr();
+	return this.getName() + this.getLname() + this.getCell();
 };
 
 module.exports = Person;
