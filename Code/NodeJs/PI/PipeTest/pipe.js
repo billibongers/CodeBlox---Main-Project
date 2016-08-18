3fa624b28bc904da721b8902607197b59f61f08c
lspazzoli <lspazzoli@gmail.com>
2016-08-18 12:20:37 +0200

    A basic pipe
    this is just more code that is here for research parts are required for
    the main project

---
+++ Code/NodeJs/PI/PipeTest/pipe.js
@@ -0,0 +1,47 @@
+//Code found at http://stackoverflow.com/questions/11750041/how-to-create-a-named-pipe-in-node-js
+//Code is used to run a client and a server connection working through a pipe
+//tested and works locally
+
+var net = require('net');
+
+var PIPE_NAME = "mypipe";
+var PIPE_PATH = "\\\\.\\pipe\\" + PIPE_NAME;
+
+var L = console.log;
+
+var server = net.createServer(function(stream) {
+    L('Server: on connection')
+
+    stream.on('data', function(c) {
+        L('Server: on data:', c.toString());
+    });
+
+    stream.on('end', function() {
+        L('Server: on end')
+        server.close();
+    });
+
+    stream.write('Take it easy!');
+});
+
+server.on('close',function(){
+    L('Server: on close');
+})
+
+server.listen(PIPE_PATH,function(){
+    L('Server: on listening');
+})
+
+// == Client part == //
+var client = net.connect(PIPE_PATH, function() {
+    L('Client: on connection');
+})
+
+client.on('data', function(data) {
+    L('Client: on data:', data.toString());
+    client.end('Thanks!');
+});
+
+client.on('end', function() {
+    L('Client: on end');
+})
