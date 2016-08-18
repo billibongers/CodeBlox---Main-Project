a1f631c239ab77c591467d9cbd9893a13cb19c52
lspazzoli <lspazzoli@gmail.com>
2016-08-18 11:40:49 +0200

    RBP Server
    Added server for the pi that will be used to assist with builing
    functionality to the pi , still needs to be tested

---
+++ Code/NodeJs/PI/Server.js
@@ -0,0 +1,42 @@
+//This server will be hosted on the pi to test some of the classes such as 
+//the gate opening , for the final product the pi will only host a client
+// additional information on this server can be found at https://nodejs.org/en/docs/guides/anatomy-of-an-http-transaction/
+var http = require('http');
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // BEGINNING OF NEW STUFF
+
+    response.on('error', function(err) {
+      console.error(err);
+    });
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+
+    var responseBody = {
+      headers: headers,
+      method: method,
+      url: url,
+      body: body
+    };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
+
+    // END OF NEW STUFF
+  });
+}).listen(8080);
