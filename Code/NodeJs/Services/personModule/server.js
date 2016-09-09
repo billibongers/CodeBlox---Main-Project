fb84e0f066eee95ec4268c13bca13d2ee146aab0
Bilal Muhammad <billibongers@gmail.com>
2016-09-09 04:18:51 +0200

    Trying to add db connection, Lethabo and Bilal

--- Code/NodeJs/Services/personModule/server.js
+++ Code/NodeJs/Services/personModule/server.js
@@ -1,12 +1,27 @@
 var express = require("express");
 var app = express();
 var mongo = require('mongojs');
-var db = mongo('dbPerson', ['persons']);
+//var db = mongo('dbPerson', ['persons']);
 var bodyParser = require("body-parser");
 var bcrypt = require('bcrypt');
 var SALT = "$2a$10$.b.ov84QXKdDd2CC7PZPl.";
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log("Database connection ready");
+
 
 app.use(bodyParser.urlencoded());
 var router = express.Router();
